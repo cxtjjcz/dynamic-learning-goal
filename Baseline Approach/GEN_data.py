fac1548156aefa409ec4ad352d094fa1142ad987
ziang chu <ziangchu@ziangs-MacBook-Pro.local>
2019-06-06 23:45:32 -0400

    decision forest approach

--- Baseline Approach/GEN_data.py
+++ Baseline Approach/GEN_data.py
@@ -28,9 +28,11 @@ def generate_random_dag(nodes, density):
 Feature space should include Student id, ethnicity, gender, age, etc.
 Data should include set of knowledge points students already know; Trace of learning: e.g. (D, 3mins), (E, 5mins) .etc. 
 """
-N = 15 #number of student
-M = 30 #number of knowledge points
-DIFFICULTY = [random.randint(3,10) for i in range(M)]
+N = 100 #number of student
+M = 25 #number of knowledge points
+MIN_TIME = 5
+MAX_TIME = 100
+TIME_CONSUMED = [random.randint(MIN_TIME,MAX_TIME) for i in range(M)]
 DIFF_VAR = 2
 DENSITY = 0.4
 DEPENDENCY = generate_random_dag(M, DENSITY)
@@ -45,12 +47,12 @@ GENDER = ["Male", "Female"]
 AGEMIN = 18
 AGEMAX = 25
 
-MIN_LEARNINGSTEP = 6
+MIN_LEARNINGSTEP = 5
 MAX_LEARNINGSTEP = 20
 
 with open("generated_data.csv", mode="w") as generated_data:
     dwriter = csv.writer(generated_data, delimiter=",")
-    dwriter.writerow(["ID", "ethnicity", "gender", "age", "Trace of learning"])
+    dwriter.writerow(["ID", "ethnicity", "gender", "age", "Trace of learning", "Learning Time"])
     for i in range(N):
         ethnicity = ETHNICITY[random.randint(0,len(ETHNICITY) - 1)]
         gender = GENDER[random.randint(0,len(GENDER) - 1)]
@@ -58,11 +60,13 @@ with open("generated_data.csv", mode="w") as generated_data:
         avail_kps = [n for n,d in DEPENDENCY.in_degree() if d == 0]
         visited = []
         learning_sequence = []
+        learning_time = []
         steps = random.randint(MIN_LEARNINGSTEP, MAX_LEARNINGSTEP)
         for s in range(steps):
             cur_node = avail_kps[random.randint(0, len(avail_kps) - 1)]
-            time = random.randint(DIFFICULTY[cur_node]-DIFF_VAR, DIFFICULTY[cur_node]+DIFF_VAR)
-            learning_sequence.append((cur_node, time))
+            time = random.randint(TIME_CONSUMED[cur_node]-DIFF_VAR, TIME_CONSUMED[cur_node]+DIFF_VAR)
+            learning_sequence.append(cur_node)
+            learning_time.append(time)
             visited.append(cur_node)
             avail_kps.remove(cur_node)
             for (_,y) in DEPENDENCY.out_edges(cur_node):
@@ -73,4 +77,4 @@ with open("generated_data.csv", mode="w") as generated_data:
                         break
                 if avail: 
                     avail_kps.append(y)
-        dwriter.writerow([str(i), ethnicity, gender, str(age), str(learning_sequence)])
+        dwriter.writerow([str(i), ethnicity, gender, str(age), learning_sequence, learning_time])
